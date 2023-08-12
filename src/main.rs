110dc759ec790782af05e9e50da1481e74983946
inalone <me@inal.one>
2023-08-12 08:55:31 +0100

    Add greet endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use axum::extract::Path;
+use axum::routing::get;
+use axum::Router;
+use std::net::SocketAddr;
+
+async fn greet(Path(name): Path<String>) -> String {
+    format!("Hello {}!", name)
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/:name", get(greet));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
